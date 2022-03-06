43290156b0548805bd8bdebbc4d3600e8774cbc8
CristhianNY <cristhian4545gmail.com>
2022-03-06 17:10:35 -0500

    botton sheet to show product details

--- app/src/main/java/com/mercadolibre/cristhianbonilla/mercadolibre/product_list/ProductListFragment.kt
+++ app/src/main/java/com/mercadolibre/cristhianbonilla/mercadolibre/product_list/ProductListFragment.kt
@@ -4,14 +4,20 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
+import android.view.WindowManager
+import android.widget.FrameLayout
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.ViewModelProvider
+import com.bumptech.glide.Glide
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.mercadolibre.cristhianbonilla.domain.model.ProductModel
 import com.mercadolibre.cristhianbonilla.mercadolibre.MainViewModel
 import com.mercadolibre.cristhianbonilla.mercadolibre.R
 import com.mercadolibre.cristhianbonilla.mercadolibre.databinding.FragmentProductListBinding
+import com.mercadolibre.cristhianbonilla.mercadolibre.databinding.ProductDetailBinding
 import com.mercadolibre.cristhianbonilla.support.config.binding.fragmentBinding
+import com.mercadolibre.cristhianbonilla.support.config.toCurrency
 import com.xwray.groupie.GroupieAdapter
 import dagger.android.support.DaggerFragment
 import javax.inject.Inject
@@ -22,6 +28,13 @@ class ProductListFragment : DaggerFragment() {
     private val adapter by lazy { GroupieAdapter() }
 
     private val binding by fragmentBinding<FragmentProductListBinding>(R.layout.fragment_product_list)
+
+
+    private val productDetailsBottomSheetBinding by fragmentBinding<ProductDetailBinding>(
+        R.layout.product_detail
+    )
+
+    private var productDetailsBottomShee: BottomSheetDialog? = null
 
     @Inject
     lateinit var factory: ViewModelProvider.Factory
@@ -39,6 +52,7 @@ class ProductListFragment : DaggerFragment() {
         super.onViewCreated(view, savedInstanceState)
         setUpProductsRecyclerView()
         showProducts()
+        setUpProductDetailsBottomSheet()
     }
 
     private fun showProducts() {
@@ -52,10 +66,46 @@ class ProductListFragment : DaggerFragment() {
     }
 
     private fun showProductDetails(productModel: ProductModel) {
-        Toast.makeText(requireContext(), "show Details", Toast.LENGTH_LONG).show()
+        productDetailsBottomSheetBinding.tvTitle.text = productModel.title
+        productDetailsBottomSheetBinding.tvPriceValue.text = productModel.price.toCurrency()
+        productDetailsBottomSheetBinding.tvQueantityValue.text =
+            productModel.availableQuantity.toString()
+        productDetailsBottomSheetBinding.tvYesOrNot.text =
+            (if (productModel.acceptsMercadopago) requireContext().getString(R.string.yes) else requireContext().getString(
+                R.string.not
+            ))
+
+        productDetailsBottomSheetBinding.ivProduct.run {
+            Glide.with(context)
+                .load(productModel.thumbnail)
+                .centerCrop()
+                .placeholder(R.drawable.ic_mercadolibre)
+                .error(R.drawable.ic_launcher_background)
+                .fallback(com.google.android.material.R.drawable.ic_clock_black_24dp)
+                .into(this)
+        }
+
+        productDetailsBottomShee?.show()
+
     }
 
     private fun setUpProductsRecyclerView() {
         binding.rvProductList.adapter = adapter
+    }
+
+    private fun setUpProductDetailsBottomSheet() {
+        if (productDetailsBottomShee == null) {
+            productDetailsBottomShee =
+                BottomSheetDialog(requireContext(), R.style.BottomSheetDialog)
+            productDetailsBottomShee?.window?.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+            productDetailsBottomSheetBinding?.let { productDetailsBottomShee?.setContentView(it.root) }
+
+            productDetailsBottomShee?.findViewById<FrameLayout>(com.google.android.material.R.id.design_bottom_sheet)
+                ?.let {
+                    BottomSheetBehavior.from(it).state = BottomSheetBehavior.STATE_EXPANDED
+                }
+
+            (productDetailsBottomSheetBinding?.root?.parent as View).setBackgroundResource(R.drawable.background_bottom_sheet)
+        }
     }
 }
