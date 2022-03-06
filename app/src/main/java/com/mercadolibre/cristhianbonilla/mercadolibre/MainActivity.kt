6eec42e45f120c26b094c5f2f090038cc7785ea5
CristhianNY <cristhian4545gmail.com>
2022-03-06 14:16:17 -0500

    add search fragment desing

--- app/src/main/java/com/mercadolibre/cristhianbonilla/mercadolibre/MainActivity.kt
+++ app/src/main/java/com/mercadolibre/cristhianbonilla/mercadolibre/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.mercadolibre.cristhianbonilla.mercadolibre
 
 import android.os.Bundle
+import com.mercadolibre.cristhianbonilla.mercadolibre.databinding.ActivityMainBinding
+import com.mercadolibre.cristhianbonilla.support.config.binding.activityBinding
 import dagger.android.support.DaggerAppCompatActivity
 
 class MainActivity : DaggerAppCompatActivity() {
+
+    private val binding by activityBinding<ActivityMainBinding>(R.layout.activity_main)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding.run {
+            setContentView(root)
+        }
     }
 }
